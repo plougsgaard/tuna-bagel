b63e708f7aa7b0fbfabbda6db90c70d04e30a819
plougsgaard <thomas@plougsgaard.dk>
2016-03-17 23:33:37 +0100

    Refactor components

--- src/components/forms/util.js
+++
@@ -1,4 +0,0 @@
-export const validateRequiredFields = (optionalFields = []) => 
-  (values) => 
-    _.mapValues(values, (v, k) => 
-      optionalFields.indexOf(k) === -1 && !v && 'Required')
---
+++ src/pages/forgot/ResetConfirm.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import _ from 'lodash'
+import { Link } from 'react-router'
+import { reduxForm } from 'redux-form'
+
+import { validateRequiredFields } from '../../helpers/validators'
+import { FormFieldError, OneLineError } from '../../components/errors'
+
+const fields = ['email', 'password']
+const validate = validateRequiredFields()
+
+const BaseForm = ({
+  fields: {
+    email,
+    password
+  },
+  handleSubmit,
+  submitting,
+  error
+}) => (
+  <form className='form-horizontal' onSubmit={handleSubmit}>
+    <fieldset className=''>
+      <legend>Confirm Reset</legend>
+      <div className='form-group'>
+        <label className='col-lg-2 control-label' forName='inputEmail'>
+          Email
+        </label>
+        <div className='col-lg-10'>
+          <input
+            type='text'
+            className='form-control'
+            id='inputEmail'
+            placeholder='somebody@example.com'
+            autoFocus
+            {...email} />
+          <FormFieldError {...email} />
+        </div>
+      </div>
+      <div className='form-group'>
+        <label className='col-lg-2 control-label' forName='inputPassword'>
+          Password
+        </label>
+        <div className='col-lg-10'>
+          <input
+            type='password'
+            className='form-control'
+            id='inputPassword'
+            {...password} />
+            <FormFieldError {...password} />
+        </div>
+      </div>
+      <div className='form-group'>
+        {error && OneLineError({ ...error })}
+      </div>
+      <div className='form-group'>
+        <div className='col-lg-10 col-lg-offset-2'>
+          <Link to='/login' className='btn btn-default' disabled={submitting}>Cancel</Link>
+          {' '}
+          <button type='submit' className='btn btn-primary' disabled={submitting}>Confirm</button>
+        </div>
+      </div>
+    </fieldset>
+  </form>
+)
+
+export default reduxForm({
+  form: 'requestConfirm',
+  fields,
+  validate
+})(BaseForm)
