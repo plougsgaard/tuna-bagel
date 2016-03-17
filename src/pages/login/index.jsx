f3b04f1c1ff68a974e9ee322fc0cad84ef570659
plougsgaard <thomas@plougsgaard.dk>
2016-03-17 22:56:04 +0100

    Refactor smart->semismart

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -6,33 +6,27 @@ import { handleSubmitLogin } from '../../redux/reducers/session'
 
 import LoginForm from './LoginForm'
 
-class LoginPage extends Component {
-  constructor (props) {
-    super(props)
-  }
-  static contextTypes = { router: PropTypes.object }
-  render () {
-    const nextPath = _.get(this.props, 'location.state.nextPathname')
-    return (
-      <div>
-        <div className='jumbotron'>
-          <h1>Sign in if you please</h1>
-          <p>No it's a mistake, <Link to='/landing'>take me back</Link> to the landing page!</p>
+const LoginPage = ( props ) => {
+  const nextPath = _.get(props, 'location.state.nextPathname')
+  return (
+    <div>
+      <div className='jumbotron'>
+        <h1>Sign in if you please</h1>
+        <p>No it's a mistake, <Link to='/landing'>take me back</Link> to the landing page!</p>
+      </div>
+      <div className='row'>
+        <div className='col-lg-6'>
+          <LoginForm onSubmit={handleSubmitLogin(nextPath)}/>
         </div>
-        <div className='row'>
-          <div className='col-lg-6'>
-            <LoginForm onSubmit={handleSubmitLogin(nextPath)}/>
-          </div>
-          <div className='col-lg-6'>
-            <blockquote className='blockquote-reverse'>
-              <p>One does not simply <Link to='/forgot'>forget his password</Link>.</p>
-              <small>Eddard Stark of <cite>Winterfell</cite></small>
-            </blockquote>
-          </div>
+        <div className='col-lg-6'>
+          <blockquote className='blockquote-reverse'>
+            <p>One does not simply <Link to='/forgot'>forget his password</Link>.</p>
+            <small>Eddard Stark of <cite>Winterfell</cite></small>
+          </blockquote>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default connect()(LoginPage)
