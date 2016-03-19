887b5572b96222b9b8d0795c8b386e97b0ef3056
plougsgaard <thomas@plougsgaard.dk>
2016-03-19 23:08:30 +0100

    Autosuggest - just tryin'

--- src/pages/foods/AddFood.jsx
+++ src/pages/foods/AddFood.jsx
@@ -3,30 +3,59 @@ import _ from 'lodash'
 import { Link } from 'react-router'
 import { reduxForm } from 'redux-form'
 
+import Autosuggest from 'react-autosuggest'
+
 import { validateRequiredFields } from '../../helpers/validators'
 import { FormFieldError, OneLineError } from '../../components/errors'
 
-const fields = ['name', 'calories']
+const fields = [
+  'name',
+  'brand',
+  'calories',
+  'carbohydrates',
+  'sugars',
+  'proteins',
+  'fat',
+  'saturated',
+  'fibres',
+  'salt'
+]
 const validate = validateRequiredFields()
 
 const TextGroup = ({ label, entity }) => {
   const inputId = `input${label}`
   return (
     <div className='form-group'>
-      <label className='col-lg-2 control-label' forName={inputId}>
-        Name
+      <label className='control-label' forName={inputId}>
+        {label}
       </label>
-      <div className='col-lg-10'>
-        <input
-          type='text'
-          className='form-control'
-          id={inputId}
-          placeholder='Encheladas'
-          autoFocus
-          {...entity} />
-        <FormFieldError {...entity} />
-      </div>
+      <input
+        type='text'
+        className='form-control'
+        id={inputId}
+        placeholder=''
+        {...entity} />
     </div>
+  )
+}
+
+const TypeaheadGroup = ({ entity }) => {
+  const suggestions = [
+    { name: 'Albania', value: 'value--Albania' },
+    { name: 'Algeria', value: 'value--Algeria' },
+    { name: 'Andorra', value: 'value--Andorra' },
+    { name: 'Angola', value: 'value--Angola' }
+  ]
+  return (
+    <Autosuggest
+      suggestions={suggestions}
+      getSuggestionValue={(s) => s.name}
+      renderSuggestion={(s) => <span style={{backgroundColor: 'red'}}>{s.name}</span>}
+      inputProps={{
+        ...entity,
+        className: 'form-control',
+        value: entity.value || ''
+      }} />
   )
 }
 
@@ -34,17 +63,17 @@ const NumberGroup = ({ entity, label, unit }) => {
   const inputId = `input${label}`
   return (
     <div className='form-group'>
-      <label className='col-sm-6 control-label' forName={inputId}>
+      <label className='control-label' forName={inputId}>
         {label}
       </label>
-      <div className='col-sm-6'>
-        <input
-          type='number'
-          className='form-control'
-          id={inputId}
-          {...entity} />
-          <FormFieldError {...entity} />
-        <span className='input-group-addon'>{unit || 'g'}</span>
+      <div className='input-group'>
+      <input
+        type='number'
+        className='form-control'
+        id={inputId}
+        {...entity} />
+        <FormFieldError {...entity} />
+      <span className='input-group-addon'>{unit || 'g'}</span>
       </div>
     </div>
   )
@@ -67,52 +96,53 @@ const BaseForm = ({
   submitting,
   error
 }) => (
-  <form className='form-horizontal' onSubmit={handleSubmit}>
+  <form className='form' onSubmit={handleSubmit}>
     <fieldset className=''>
       <div className='row'>
         <div className='col-md-6'>
           <TextGroup label='Description' entity={name}/>
         </div>
         <div className='col-md-6'>
-          <TextGroup label='Brand' entity={brand}/>
+          <TypeaheadGroup label='Brand' entity={brand}/>
         </div>
       </div>
       <div className='row'>
-        <div className='col-sm-6'>
+        <div className='col-sm-3'>
           <NumberGroup label='Calories' entity={calories} unit='kcal' />
         </div>
-        <div className='col-sm-6'>
+        <div className='col-sm-3'>
           <NumberGroup label='Carbohydrates' entity={carbohydrates} />
         </div>
-      </div>
-      <div className='row'>
-        <div className='col-sm-6'>
+      {/*</div>
+      <div className='row'>*/}
+        <div className='col-sm-3'>
           <NumberGroup label='Sugars' entity={sugars} />
         </div>
-        <div className='col-sm-6'>
+        <div className='col-sm-3'>
           <NumberGroup label='Proteins' entity={proteins} />
         </div>
       </div>
       <div className='row'>
-        <div className='col-sm-6'>
+        <div className='col-sm-3'>
           <NumberGroup label='Fat' entity={fat} />
         </div>
-        <div className='col-sm-6'>
+        <div className='col-sm-3'>
           <NumberGroup label='Saturated' entity={saturated} />
         </div>
-      </div>
-      <div className='row'>
-        <div className='col-sm-6'>
+      {/*</div>
+      <div className='row'>*/}
+        <div className='col-sm-3'>
           <NumberGroup label='Fibres' entity={fibres} />
         </div>
-        <div className='col-sm-6'>
+        <div className='col-sm-3'>
           <NumberGroup label='Salt' entity={salt} />
         </div>
       </div>
       <div className='row'>
-        <div className='col-md-12'>
+        <div className='col-md-10'></div>
+        <div className='col-md-2'>
           <div className='form-group'>
-            <div className='col-lg-10 col-lg-offset-2'>
+            <div className='col-lg-2 col-lg-offset-2'>
               <button type='submit' className='btn btn-success' disabled={submitting}>Create</button>
             </div>
           </div>
